af986cb4e5709054f51685b1aaaf1f05d077fd90
MartinaBoazzo1 <martina.boazzo@solucionet.com.ar>
2022-10-21 16:36:08 -0300

    subiendo mercadoliebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+//Unificamos rutas
+
+const publicPath = path.resolve(__dirname,'./public')
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.listen(3000,()=>{
+    console.log("Servidor corriendo")
+});
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/views/home.html');
+})
+app.get('/register',(req,res)=>{
+    res.sendFile(__dirname +'/views/register.html');
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname +'/views/login.html');
+})
