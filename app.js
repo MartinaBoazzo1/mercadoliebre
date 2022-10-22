9c69a42f3536603124741f210e065e35d12246a8
MartinaBoazzo1 <martina.boazzo@solucionet.com.ar>
2022-10-21 22:14:50 -0300

    subiendo 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname,'./public')
 //app.use(express.static('public'));
 app.use(express.static(path.join(__dirname,'/public')));
 
-app.listen(process.env.PORT || 4200,()=>{
+app.listen(process.env.PORT || 6000,()=>{
     console.log("Servidor corriendo")
 });
 app.get('/',(req,res)=>{
